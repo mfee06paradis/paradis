745537bbebb8b7a80bccfbc52b696276a7a708e6
AllEN <allen19842608@hotmail.com>
2020-05-03 11:46:40 +0800

    優惠頁面

--- src/pages/Discount.js
+++ src/pages/Discount.js
@@ -1,43 +1,63 @@
 import React from 'react'
-import '../styles/discount.css'
+import '../styles/discount.scss'
 function discont() {
+  const menu_title = (eng, chs) => {
+    return (
+      <>
 
+        <div className="menu-title">
+          <p>{eng}</p>
+          <p>{chs}</p>
+        </div>
 
+      </>
+    )
+  }
   return (
     <>
-      <div class="container">
-        <div class="row ">
-          <div class="row col-12 disMenu1">
-            <div class="col-6 text-center disBtn1-1">
-              <button className="btn-white">當季優惠</button>
+      <div className="bg-white">
+        <div className="container">
+          <div className="row ">
+
+            <div className="row col-12 disMenu1">
+              <div className="col-6 text-center disBtn1-1">
+                <button className="btn-white">當季優惠</button>
+
+              </div>
+              <div className="col-6 text-center disBtn1-2">
+                <button className="btn-white">現正優惠</button>
+              </div>
+
             </div>
-            <div class="col-6 text-center disBtn1-2">
-              <button className="btn-white">現正優惠</button>
-        </div>
-            </div>
-            <div class="row col-md-2 disMenu2">
-              <div class="col-md-12 text-center">
-                <button>
-                <img src="/img/loge1.svg" width="30px" height="30px" class="disBtn2-1"></img>
-                <p>Seson</p>
-                <p>當季優惠</p>
+
+            <div className="row col-md-2 disMenu2">
+              <div className="col-md-12 text-center">
+                <button className="bg-white">
+                  <div className="menu-img">
+                    {menu_title('Seson', '當季優惠')}
+                    <hr></hr>
+                  </div>
                 </button>
-                <hr></hr>
-                <img src="/img/loge1.svg" width="30px" height="30px" class="disBtn2-2"></img>
-                <p>Now On Sale</p>
-                <p>現正優惠</p>
-                <hr></hr>
+
+                <button className="bg-white">
+                <div className="menu-img">
+                  {menu_title('Now On Sale', '現正優惠')}
+                  <hr></hr>
+                  </div>
+                </button>
+
               </div>
             </div>
-            <div class="col-12 col-md-10 ">
+            <div className="col-12 col-md-10 ">
 
-              <div class="disImg">
-                <img src="http://fakeimg.pl/800x400/F4A7B9/282828/?text=DISCONT" class="img-fluid" alt="" />
+              <div className="disImg">
+                <img src="http://fakeimg.pl/800x400/F4A7B9/282828/?text=DISCONT" className="img-fluid" alt="" />
               </div>
 
             </div>
           </div>
         </div>
+      </div>
     </>
   )
 
