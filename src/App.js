5d4c12575c401e8b2fe39e6641e34a5ac079e49e
mfee06paradis <64397614+mfee06paradis@users.noreply.github.com>
2020-05-13 14:35:36 +0800

    5/13 14:35  新增我的訂單＆

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import React, { useState } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useState } from 'react';
 
-import Uielements from './pages/Uielements'
-import Home from './pages/Home'
-import Brands from './pages/Brands'
-import Product from './pages/Product'
-import Discount from './pages/Discount'
-import Discounts from './pages/Discounts'
-import DiscountList from './components/DiscountList'
+import Uielements from './pages/Uielements';
+import Home from './pages/Home';
+import Brands from './pages/Brands';
+import Product from './pages/Product';
+import Discount from './pages/Discount';
+import Discounts from './pages/Discounts';
+import DiscountList from './components/DiscountList';
 import Makeup from './pages/Makeup';
-import Lesson from './pages/Lesson'
+import Lesson from './pages/Lesson';
 import AdaptiveTest from './pages/AdaptiveTest';
-import Cart from './pages/Cart'
-import Ani from './pages/Ani'
-import Member from './pages/Member'
-import MemberAdd from './pages/MemberAdd'
-import CreditCard from './pages/CreditCard'
-import Account from './pages/Account'
-import EditAccount from './pages/EditAccount'
-import NotFoundPage from './pages/NotFoundPage'
+import Cart from './pages/Cart';
+import Ani from './pages/Ani';
+import Member from './pages/Member';
+import MemberAdd from './pages/MemberAdd';
+import CreditCard from './pages/CreditCard';
+import Account from './pages/Account';
+import EditAccount from './pages/EditAccount';
+import MyOrders from './pages/MyOrders';
+import MyFavorite from './pages/MyFavorite';
+import NotFoundPage from './pages/NotFoundPage';
 
-import Menu from './components/Menu'
-import Footer from './components/Footer'
+import Menu from './components/Menu';
+import Footer from './components/Footer';
 
 function App() {
-  const [isAuth, setIsAuth] = useState(false)
-  const [cartNumber, setCartNumber] = useState(0)
+  const [isAuth, setIsAuth] = useState(false);
+  const [cartNumber, setCartNumber] = useState(0);
 
   return (
-
-
     <Router>
       <>
-        <Menu isAuth={isAuth} setIsAuth={setIsAuth} cartNumber={cartNumber} setCartNumber={setCartNumber}/>
+        <Menu
+          isAuth={isAuth}
+          setIsAuth={setIsAuth}
+          cartNumber={cartNumber}
+          setCartNumber={setCartNumber}
+        />
         <Switch>
           <Route exact path="/">
             <Ani />
@@ -88,18 +93,23 @@ function App() {
           <Route path="/EditAccount">
             <EditAccount />
           </Route>
+          <Route path="/myorders">
+            <MyOrders />
+          </Route>
+          <Route path="/MyFavorite">
+            <MyFavorite />
+          </Route>
           <Route exact path="/">
             <Ani />
           </Route>
           <Route path="*">
             <NotFoundPage />
           </Route>
-
         </Switch>
         <Footer />
       </>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
